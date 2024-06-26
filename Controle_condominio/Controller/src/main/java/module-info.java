a533392fecb30bab7d288236afc5d0067ae9c43a
brunoloubraz <brunolou.braz@gmail.com>
2024-06-26 19:57:22 -0300

    'projeto'

---
+++ Controle_condominio/Controller/src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com.project.controller {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.project.controller to javafx.fxml;
+    exports com.project.controller;
+}
